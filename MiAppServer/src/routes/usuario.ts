2ca7a890fa19f905d3b20e9837aa18dc4181987a
LEIBA CALERO Javier <leivahenry5@gmail.com>
2026-04-29 10:05:52 -0600

    29 de abril, 2026

--- MiAppServer/src/routes/usuario.ts
+++ MiAppServer/src/routes/usuario.ts
@@ -1,18 +1,14 @@
-import { UsuarioController } from "../controllers/UsuarioController";
-import { Router } from "express";
-import { validateRequest } from "../middleware/validateRequest";
-import { createUsuarioDto, updateUsuarioDto } from "../dtos/UsuarioDto";
+import { UsuarioController } from '../controllers/UsuarioController';
+import { Router } from 'express';
+import { validateRequest } from '../middleware/validateRequest';
+import { createUsuarioDto, updateUsuarioDto } from '../dtos/UsuarioDto';
 
 const ROUTES = Router();
 
-ROUTES.get("/", UsuarioController.getUsuarios);
-ROUTES.get("/:id", UsuarioController.getUsuarioById);
-ROUTES.post(
-  "/",
-  validateRequest({ body: createUsuarioDto }),
-  UsuarioController.createUsuarios,
-);
-ROUTES.patch("/:id", validateRequest({ body: updateUsuarioDto }), UsuarioController.updateUsuario);
-ROUTES.delete("/:id", UsuarioController.deleteUsuario);
+ROUTES.get('/', UsuarioController.getUsuarios);
+ROUTES.get('/:id', UsuarioController.getUsuarioById);
+ROUTES.post('/', validateRequest({ body: createUsuarioDto }), UsuarioController.createUsuarios);
+ROUTES.patch('/:id', validateRequest({ body: updateUsuarioDto }), UsuarioController.updateUsuario);
+ROUTES.delete('/:id', UsuarioController.deleteUsuario);
 
 export default ROUTES;
